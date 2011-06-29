ecad121fe15b50fdd28245615f203a70a40e8962
Cameron Hart <cam@bitshifter.net.nz>
2011-06-29 21:36:38 +1200

    BasicImage example compiles and links on Linux with CMake.

--- Core/Build/Linux/PolycodeView.cpp
+++ Core/Build/Linux/PolycodeView.cpp
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long getThreadID() {
+        return (long)pthread_self();
+}
+
 PolycodeView::PolycodeView(const char *title) : PolycodeViewBase() {
 	windowTitle = title;
 	windowData = &windowTitle;
