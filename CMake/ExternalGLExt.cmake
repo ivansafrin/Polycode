4d3959f6828f1c1aff100d16c98d82f1e836def5
Cameron Hart <cam@bitshifter.net.nz>
2011-07-16 10:48:09 +1200

    Tidied up glext.g and wglext.h downloading some more.

--- CMake/ExternalGLExt.cmake
+++ CMake/ExternalGLExt.cmake
@@ -7,13 +7,5 @@ IF(NOT EXISTS ${POLYCODE_DEPS_DOWNLOAD_DIR}/glext.h)
         SHOW_PROGRESS)
 ENDIF()
 
-IF(NOT EXISTS ${POLYCODE_DEPS_DOWNLOAD_DIR}/wglext.h)
-    MESSAGE("Downloading http://www.opengl.org/registry/api/wglext.h")
-    FILE(DOWNLOAD http://www.opengl.org/registry/api/wglext.h 
-        ${POLYCODE_DEPS_DOWNLOAD_DIR}/wglext.h
-        SHOW_PROGRESS)
-ENDIF()
-
-ADD_CUSTOM_TARGET(glext
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${POLYCODE_DEPS_DOWNLOAD_DIR}/glext.h ${POLYCODE_DEPS_CORE_PREFIX}/include/GL/glext.h
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${POLYCODE_DEPS_DOWNLOAD_DIR}/wglext.h ${POLYCODE_DEPS_CORE_PREFIX}/include/GL/wglext.h)
+ADD_CUSTOM_TARGET(wglext
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${POLYCODE_DEPS_DOWNLOAD_DIR}/glext.h ${POLYCODE_DEPS_CORE_PREFIX}/include/GL/glext.h)
