c802140464c55f9ce5137994805da547e24f03f5
Ivan Safrin <ivansafrin@gmail.com>
2011-08-13 18:19:52 -0400

    Added documentation folder to the repository

---
+++ Documentation/Doxygen/output/standalone/Polycode/html/navtree.js
@@ -0,0 +1,407 @@
+var NAVTREE =
+[
+  [ "Polycode", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "Polycode::BezierCurve", "class_polycode_1_1_bezier_curve.html", null ],
+      [ "Polycode::BezierPathTween", "class_polycode_1_1_bezier_path_tween.html", null ],
+      [ "Polycode::BezierPoint", "class_polycode_1_1_bezier_point.html", null ],
+      [ "Polycode::Bone", "class_polycode_1_1_bone.html", null ],
+      [ "Polycode::BoneAssignment", "class_polycode_1_1_bone_assignment.html", null ],
+      [ "Polycode::Camera", "class_polycode_1_1_camera.html", null ],
+      [ "Polycode::Color", "class_polycode_1_1_color.html", null ],
+      [ "Polycode::Config", "class_polycode_1_1_config.html", null ],
+      [ "Polycode::Core", "class_polycode_1_1_core.html", null ],
+      [ "Polycode::CoreInput", "class_polycode_1_1_core_input.html", null ],
+      [ "Polycode::CoreServices", "class_polycode_1_1_core_services.html", null ],
+      [ "Polycode::Data", "class_polycode_1_1_data.html", null ],
+      [ "Polycode::Entity", "class_polycode_1_1_entity.html", null ],
+      [ "Polycode::Event", "class_polycode_1_1_event.html", null ],
+      [ "Polycode::EventDispatcher", "class_polycode_1_1_event_dispatcher.html", null ],
+      [ "Polycode::EventHandler", "class_polycode_1_1_event_handler.html", null ],
+      [ "Polycode::FontManager", "class_polycode_1_1_font_manager.html", null ],
+      [ "Polycode::Image", "class_polycode_1_1_image.html", null ],
+      [ "Polycode::InputEvent", "class_polycode_1_1_input_event.html", null ],
+      [ "Polycode::MaterialManager", "class_polycode_1_1_material_manager.html", null ],
+      [ "Polycode::Matrix4", "class_polycode_1_1_matrix4.html", null ],
+      [ "Polycode::Mesh", "class_polycode_1_1_mesh.html", null ],
+      [ "Polycode::Object", "class_polycode_1_1_object.html", null ],
+      [ "Polycode::ObjectEntry", "class_polycode_1_1_object_entry.html", null ],
+      [ "Polycode::ParticleEmitter", "class_polycode_1_1_particle_emitter.html", null ],
+      [ "Polycode::Perlin", "class_polycode_1_1_perlin.html", null ],
+      [ "Polycode::Polygon", "class_polycode_1_1_polygon.html", null ],
+      [ "Polycode::Quaternion", "class_polycode_1_1_quaternion.html", null ],
+      [ "Polycode::Rectangle", "class_polycode_1_1_rectangle.html", null ],
+      [ "Polycode::RenderDataArray", "class_polycode_1_1_render_data_array.html", null ],
+      [ "Polycode::Renderer", "class_polycode_1_1_renderer.html", null ],
+      [ "Polycode::Resource", "class_polycode_1_1_resource.html", null ],
+      [ "Polycode::ResourceManager", "class_polycode_1_1_resource_manager.html", null ],
+      [ "Polycode::Scene", "class_polycode_1_1_scene.html", null ],
+      [ "Polycode::SceneEntity", "class_polycode_1_1_scene_entity.html", null ],
+      [ "Polycode::SceneLabel", "class_polycode_1_1_scene_label.html", null ],
+      [ "Polycode::SceneLight", "class_polycode_1_1_scene_light.html", null ],
+      [ "Polycode::SceneLine", "class_polycode_1_1_scene_line.html", null ],
+      [ "Polycode::SceneMesh", "class_polycode_1_1_scene_mesh.html", null ],
+      [ "Polycode::SceneParticleEmitter", "class_polycode_1_1_scene_particle_emitter.html", null ],
+      [ "Polycode::ScenePrimitive", "class_polycode_1_1_scene_primitive.html", null ],
+      [ "Polycode::SceneRenderTexture", "class_polycode_1_1_scene_render_texture.html", null ],
+      [ "Polycode::SceneSound", "class_polycode_1_1_scene_sound.html", null ],
+      [ "Polycode::SceneSoundListener", "class_polycode_1_1_scene_sound_listener.html", null ],
+      [ "Polycode::Screen", "class_polycode_1_1_screen.html", null ],
+      [ "Polycode::ScreenEntity", "class_polycode_1_1_screen_entity.html", null ],
+      [ "Polycode::ScreenImage", "class_polycode_1_1_screen_image.html", null ],
+      [ "Polycode::ScreenLabel", "class_polycode_1_1_screen_label.html", null ],
+      [ "Polycode::ScreenLine", "class_polycode_1_1_screen_line.html", null ],
+      [ "Polycode::ScreenManager", "class_polycode_1_1_screen_manager.html", null ],
+      [ "Polycode::ScreenMesh", "class_polycode_1_1_screen_mesh.html", null ],
+      [ "Polycode::ScreenParticleEmitter", "class_polycode_1_1_screen_particle_emitter.html", null ],
+      [ "Polycode::ScreenShape", "class_polycode_1_1_screen_shape.html", null ],
+      [ "Polycode::ScreenSound", "class_polycode_1_1_screen_sound.html", null ],
+      [ "Polycode::ScreenSoundListener", "class_polycode_1_1_screen_sound_listener.html", null ],
+      [ "Polycode::ScreenSprite", "class_polycode_1_1_screen_sprite.html", null ],
+      [ "Polycode::Skeleton", "class_polycode_1_1_skeleton.html", null ],
+      [ "Polycode::SkeletonAnimation", "class_polycode_1_1_skeleton_animation.html", null ],
+      [ "Polycode::Sound", "class_polycode_1_1_sound.html", null ],
+      [ "Polycode::SoundManager", "class_polycode_1_1_sound_manager.html", null ],
+      [ "Polycode::String", "class_polycode_1_1_string.html", null ],
+      [ "Polycode::Threaded", "class_polycode_1_1_threaded.html", null ],
+      [ "Polycode::Timer", "class_polycode_1_1_timer.html", null ],
+      [ "Polycode::Tween", "class_polycode_1_1_tween.html", null ],
+      [ "Polycode::Vector2", "class_polycode_1_1_vector2.html", null ],
+      [ "Polycode::Vector3", "class_polycode_1_1_vector3.html", null ],
+      [ "Polycode::Vertex", "class_polycode_1_1_vertex.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "Polycode::BezierCurve", "class_polycode_1_1_bezier_curve.html", null ],
+      [ "Polycode::BezierPoint", "class_polycode_1_1_bezier_point.html", null ],
+      [ "Polycode::BoneAssignment", "class_polycode_1_1_bone_assignment.html", null ],
+      [ "Polycode::Color", "class_polycode_1_1_color.html", null ],
+      [ "Polycode::Config", "class_polycode_1_1_config.html", null ],
+      [ "Polycode::Data", "class_polycode_1_1_data.html", null ],
+      [ "Polycode::Entity", "class_polycode_1_1_entity.html", [
+        [ "Polycode::SceneEntity", "class_polycode_1_1_scene_entity.html", [
+          [ "Polycode::Bone", "class_polycode_1_1_bone.html", null ],
+          [ "Polycode::Camera", "class_polycode_1_1_camera.html", null ],
+          [ "Polycode::SceneLabel", "class_polycode_1_1_scene_label.html", null ],
+          [ "Polycode::SceneLight", "class_polycode_1_1_scene_light.html", null ],
+          [ "Polycode::SceneLine", "class_polycode_1_1_scene_line.html", null ],
+          [ "Polycode::SceneMesh", "class_polycode_1_1_scene_mesh.html", [
+            [ "Polycode::ScenePrimitive", "class_polycode_1_1_scene_primitive.html", null ]
+          ] ],
+          [ "Polycode::SceneParticleEmitter", "class_polycode_1_1_scene_particle_emitter.html", null ],
+          [ "Polycode::SceneSound", "class_polycode_1_1_scene_sound.html", null ],
+          [ "Polycode::SceneSoundListener", "class_polycode_1_1_scene_sound_listener.html", null ],
+          [ "Polycode::Skeleton", "class_polycode_1_1_skeleton.html", null ]
+        ] ],
+        [ "Polycode::ScreenEntity", "class_polycode_1_1_screen_entity.html", [
+          [ "Polycode::ScreenMesh", "class_polycode_1_1_screen_mesh.html", [
+            [ "Polycode::ScreenLine", "class_polycode_1_1_screen_line.html", null ],
+            [ "Polycode::ScreenShape", "class_polycode_1_1_screen_shape.html", [
+              [ "Polycode::ScreenImage", "class_polycode_1_1_screen_image.html", null ],
+              [ "Polycode::ScreenLabel", "class_polycode_1_1_screen_label.html", null ],
+              [ "Polycode::ScreenSprite", "class_polycode_1_1_screen_sprite.html", null ]
+            ] ]
+          ] ],
+          [ "Polycode::ScreenParticleEmitter", "class_polycode_1_1_screen_particle_emitter.html", null ],
+          [ "Polycode::ScreenSound", "class_polycode_1_1_screen_sound.html", null ],
+          [ "Polycode::ScreenSoundListener", "class_polycode_1_1_screen_sound_listener.html", null ]
+        ] ]
+      ] ],
+      [ "Polycode::Event", "class_polycode_1_1_event.html", [
+        [ "Polycode::InputEvent", "class_polycode_1_1_input_event.html", null ]
+      ] ],
+      [ "Polycode::EventHandler", "class_polycode_1_1_event_handler.html", [
+        [ "Polycode::EventDispatcher", "class_polycode_1_1_event_dispatcher.html", [
+          [ "Polycode::Core", "class_polycode_1_1_core.html", null ],
+          [ "Polycode::CoreInput", "class_polycode_1_1_core_input.html", null ],
+          [ "Polycode::CoreServices", "class_polycode_1_1_core_services.html", null ],
+          [ "Polycode::Scene", "class_polycode_1_1_scene.html", null ],
+          [ "Polycode::Screen", "class_polycode_1_1_screen.html", null ],
+          [ "Polycode::ScreenEntity", "class_polycode_1_1_screen_entity.html", null ],
+          [ "Polycode::ScreenManager", "class_polycode_1_1_screen_manager.html", null ],
+          [ "Polycode::Timer", "class_polycode_1_1_timer.html", null ],
+          [ "Polycode::Tween", "class_polycode_1_1_tween.html", [
+            [ "Polycode::BezierPathTween", "class_polycode_1_1_bezier_path_tween.html", null ]
+          ] ]
+        ] ],
+        [ "Polycode::SceneEntity", "class_polycode_1_1_scene_entity.html", null ]
+      ] ],
+      [ "Polycode::FontManager", "class_polycode_1_1_font_manager.html", null ],
+      [ "Polycode::Image", "class_polycode_1_1_image.html", null ],
+      [ "Polycode::MaterialManager", "class_polycode_1_1_material_manager.html", null ],
+      [ "Polycode::Matrix4", "class_polycode_1_1_matrix4.html", null ],
+      [ "Polycode::Mesh", "class_polycode_1_1_mesh.html", null ],
+      [ "Polycode::Object", "class_polycode_1_1_object.html", null ],
+      [ "Polycode::ObjectEntry", "class_polycode_1_1_object_entry.html", null ],
+      [ "Polycode::ParticleEmitter", "class_polycode_1_1_particle_emitter.html", [
+        [ "Polycode::SceneParticleEmitter", "class_polycode_1_1_scene_particle_emitter.html", null ],
+        [ "Polycode::ScreenParticleEmitter", "class_polycode_1_1_screen_particle_emitter.html", null ]
+      ] ],
+      [ "Polycode::Perlin", "class_polycode_1_1_perlin.html", null ],
+      [ "Polycode::Polygon", "class_polycode_1_1_polygon.html", null ],
+      [ "Polycode::Quaternion", "class_polycode_1_1_quaternion.html", null ],
+      [ "Polycode::Rectangle", "class_polycode_1_1_rectangle.html", null ],
+      [ "Polycode::RenderDataArray", "class_polycode_1_1_render_data_array.html", null ],
+      [ "Polycode::Renderer", "class_polycode_1_1_renderer.html", null ],
+      [ "Polycode::Resource", "class_polycode_1_1_resource.html", null ],
+      [ "Polycode::ResourceManager", "class_polycode_1_1_resource_manager.html", null ],
+      [ "Polycode::SceneRenderTexture", "class_polycode_1_1_scene_render_texture.html", null ],
+      [ "Polycode::SkeletonAnimation", "class_polycode_1_1_skeleton_animation.html", null ],
+      [ "Polycode::Sound", "class_polycode_1_1_sound.html", null ],
+      [ "Polycode::SoundManager", "class_polycode_1_1_sound_manager.html", null ],
+      [ "Polycode::String", "class_polycode_1_1_string.html", null ],
+      [ "Polycode::Threaded", "class_polycode_1_1_threaded.html", null ],
+      [ "Polycode::Vector3", "class_polycode_1_1_vector3.html", [
+        [ "Polycode::Vector2", "class_polycode_1_1_vector2.html", null ],
+        [ "Polycode::Vertex", "class_polycode_1_1_vertex.html", null ]
+      ] ]
+    ] ],
+    [ "Class Members", "functions.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
