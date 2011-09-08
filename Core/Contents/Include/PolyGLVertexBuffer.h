94c4d628d11db439939caa93ea4d9388d458e200
Cameron Hart <cam@bitshifter.net.nz>
2011-09-08 13:12:28 +1200

    Win32 compile fixes.

--- Core/Contents/Include/PolyGLVertexBuffer.h
+++ Core/Contents/Include/PolyGLVertexBuffer.h
@@ -24,13 +24,17 @@ THE SOFTWARE.
 #pragma once
 #include "PolyMesh.h"
 
+#ifdef _WINDOWS
+#include <windows.h>
+#endif
+
 #if defined(__APPLE__) && defined(__MACH__)
 #include <OpenGL/gl.h>
 #include <OpenGL/glext.h>
 #include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
-#include <GL/glu.h>
+#include <GL/glext.h>
 #endif
 
 namespace Polycode {
--- Core/Contents/Source/PolyGLSLProgram.cpp
+++ Core/Contents/Source/PolyGLSLProgram.cpp
@@ -25,13 +25,16 @@ THE SOFTWARE.
 #include "PolyVector3.h"
 #include "PolyLogger.h"
 
+#ifdef _WINDOWS
+#include <windows.h>
+#endif
+
 #if defined(__APPLE__) && defined(__MACH__)
 #include <OpenGL/gl.h>
 #include <OpenGL/glext.h>
 #include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
-#include <GL/glu.h>
 #include <GL/glext.h>
 #endif
 
