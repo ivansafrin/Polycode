c5d28364f8ff89a1fd51a3f4769daa559e76b768
cib <cib123@googlemail.com>
2013-04-12 11:09:14 +0200

    Fixes shift on windows, issue #85
    See http://stackoverflow.com/questions/15966642/how-do-you-tell-lshift-apart-from-rshift-in-wm-keydown-events

--- Core/Contents/Source/PolyWinCore.cpp
+++ Core/Contents/Source/PolyWinCore.cpp
@@ -476,13 +476,21 @@ PolyKEY Win32Core::mapKey(LPARAM lParam, WPARAM wParam) {
 						wParam = VK_RCONTROL;
 					else
 						wParam = VK_LCONTROL;
-				break;
+					break;
 				case 33:
 					if ( lParam&EXTENDED_KEYMASK )
 						wParam = VK_RMENU;
 					else
 						wParam = VK_LMENU;
 					break;
+				case VK_SHIFT:
+					// We can't tell if it's LSHIFT or RSHIFT,
+					// so use GetKeyState to tell which it is.
+					if( GetKeyState(VK_LSHIFT) ) {
+						wParam = VK_LSHIFT;
+					} else {
+						wParam = VK_RSHIFT;
+					}
 			}
 
 	return keyMap[(unsigned int)wParam];
