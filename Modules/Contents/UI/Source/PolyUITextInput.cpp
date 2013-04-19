08a4605749c7e247e1141f5abd77aad9247ac150
Ethan M <the.cosmotect@gmail.com>
2013-04-19 15:09:31 -0500

    switched to max() to make sure colEnd arg >= 0 (instead of if testing)

--- Modules/Contents/UI/Source/PolyUITextInput.cpp
+++ Modules/Contents/UI/Source/PolyUITextInput.cpp
@@ -1123,9 +1123,7 @@ void UITextInput::onKeyDown(PolyKEY key, wchar_t charCode) {
 						if(selectionCaretPosition > 0)
 							setSelection(lineOffset, selectionLine, this->caretPosition, selectionCaretPosition-1);
 					} else {
-						if (caretPosition > 0) {
-							setSelection(lineOffset, lineOffset, caretPosition, caretPosition-1);
-						}
+						setSelection(lineOffset, lineOffset, caretPosition, max(caretPosition-1, 0));
 					}
 				} else {
 					int newLineStart = lineOffset;
