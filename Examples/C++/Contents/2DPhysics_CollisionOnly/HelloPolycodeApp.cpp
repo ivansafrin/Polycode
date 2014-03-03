9f1fe955c3bf0f3d8b29f30e8183c21a7f82a547
Joachim Meyer <joachim@war-of-universe.com>
2014-03-03 19:30:43 +0100

    Rebased 2DPhysics Collision Only on screenrewrite, Known Issue: *won't change color on collision

--- Examples/C++/Contents/2DPhysics_CollisionOnly/HelloPolycodeApp.cpp
+++ Examples/C++/Contents/2DPhysics_CollisionOnly/HelloPolycodeApp.cpp
@@ -7,31 +7,31 @@ HelloPolycodeApp::HelloPolycodeApp(PolycodeView *view) {
 	CoreServices::getInstance()->getResourceManager()->addArchive("Resources/default.pak");
 	CoreServices::getInstance()->getResourceManager()->addDirResource("default", false);	
 
-	screen = new PhysicsScreen(10, 50);
+	PhysicsScene2D *scene = new PhysicsScene2D(10, 50);
 	
-	checkShape = new ScreenShape(ScreenShape::SHAPE_RECT, 90,10);
-	screen->addCollisionChild(checkShape, PhysicsScreenEntity::ENTITY_RECT);
+	checkShape = new ScenePrimitive(ScenePrimitive::TYPE_VPLANE, 90,10);
+	scene->addCollisionChild(checkShape, PhysicsScreenEntity::ENTITY_RECT);
 
 	
 	for(int i=0; i < 50; i++) {
-		ScreenShape *shape = new ScreenShape(ScreenShape::SHAPE_RECT, 30,15);
-		shape->setRotation(rand() % 360);
+		ScenePrimitive *shape = new ScenePrimitive(ScenePrimitive::TYPE_VPLANE, 30,15);
+		shape->setRoll(rand() % 360);
 		shape->setPosition(rand() % 640, rand() % 480);
-		screen->addCollisionChild(shape, PhysicsScreenEntity::ENTITY_RECT);
+		scene->addCollisionChild(shape, PhysicsScreenEntity::ENTITY_RECT);
 	}
 	
-	screen->addEventListener(this, PhysicsScreenEvent::EVENT_NEW_SHAPE_COLLISION);	
-	screen->addEventListener(this, PhysicsScreenEvent::EVENT_END_SHAPE_COLLISION);	
+	scene->addEventListener(this, PhysicsScreenEvent::EVENT_NEW_SHAPE_COLLISION);	
+	scene->addEventListener(this, PhysicsScreenEvent::EVENT_END_SHAPE_COLLISION);	
 }
 
 void HelloPolycodeApp::handleEvent(Event *e) {
-	if(e->getDispatcher() == screen) {
+	if(e->getDispatcher() == scene) {
 		PhysicsScreenEvent *pe = (PhysicsScreenEvent*)e;	
 		switch(e->getEventCode()) {
 			case PhysicsScreenEvent::EVENT_NEW_SHAPE_COLLISION:
 				if(pe->entity1 == checkShape || pe->entity2 == checkShape) {
 					pe->entity1->setColor(1.0f, 0.0f, 0.0f, 1.0f);
-					pe->entity2->setColor(1.0f, 0.0f, 0.0f, 1.0f);				
+					pe->entity2->setColor(1.0f, 0.0f, 0.0f, 1.0f);
 				}
 			break;
 			case PhysicsScreenEvent::EVENT_END_SHAPE_COLLISION:
@@ -50,6 +50,6 @@ HelloPolycodeApp::~HelloPolycodeApp() {
 bool HelloPolycodeApp::Update() {
 	Vector2 mouse = core->getInput()->getMousePosition();
 	checkShape->setPosition(mouse.x, mouse.y);
-	checkShape->setRotation(checkShape->getRotation() + (core->getElapsed() * 100));
+	checkShape->setRoll(checkShape->getRoll() + (core->getElapsed() * 100));
     return core->updateAndRender();
 }
