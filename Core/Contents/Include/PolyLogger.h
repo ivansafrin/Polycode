148efdb33a6137e223a1845f95f2f8168af68184
Joachim Meyer <joachim@war-of-universe.com>
2014-04-26 09:56:19 +0200

    Fixed Logger to log in VS Debug Output, if compiled as Debug. Documented Logger a bit.

--- Core/Contents/Include/PolyLogger.h
+++ Core/Contents/Include/PolyLogger.h
@@ -34,15 +34,34 @@ namespace Polycode {
 			String message;
 			
 	};
-
+	
+	/**
+	* Logs information to the console, should only be called through Logger::
+	*/
 	class _PolyExport Logger : public EventDispatcher {
 		public:
+			/**
+			* Default constructor
+			*/
 			Logger();
 			virtual ~Logger();
 
+			/**
+			* Dispatches LoggerEvent and calls Logger::log()
+			* @param message String to log
+			*/
 			void logBroadcast(String message);
 
+			/**
+			* Logs information to the console or debug window of VS (only available if compiled as debug)
+			* @param format c-strings to log, put the params into the first using: "%s", 2. param is set as 1. %s, 3. param is set as 2. %s, and so on
+			*/
 			static void log(const char *format, ...);
+
+			/**
+			* Logs information through wcout
+			* @param str The c-string to log
+			*/
 			static void logw(const char *str);
 	};
 }
