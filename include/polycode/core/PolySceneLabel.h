f8c949021acc098fb436d3b730ccdfe15a632848
Ivan Safrin <ivansafrin@gmail.com>
2016-06-16 18:21:30 -0400

    Fixed importer tool not saving meshes properly

--- include/polycode/core/PolySceneLabel.h
+++ include/polycode/core/PolySceneLabel.h
@@ -39,7 +39,7 @@ namespace Polycode {
 	class _PolyExport SceneLabel : public ScenePrimitive {
 		public:
 		
-			SceneLabel(const String& text, int size, const String& fontName, int amode, Number actualHeight);
+			SceneLabel(const String& text, int size, const String& fontName, int amode = 0, Number actualHeight = 0.0);
 			
 			SceneLabel(const String& text, int size, const String& fontName, int amode, Number actualHeight, bool premultiplyAlpha, const Color &backgroundColor, const Color &foregroundColor);
 			
